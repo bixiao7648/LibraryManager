ea2b672d0987b7af0b1dc10f32ad686ba59954f5
Administrator <1060594195@qq.com>
2024-10-08 09:50:11 +0800

    import AspectJ

---
+++ app/src/main/java/aspect/AspectTest.java
@@ -0,0 +1,37 @@
+package aspect;
+
+import com.orhanobut.logger.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by user on 18-8-7.
+ */
+
+@Aspect
+public class AspectTest {
+
+    private Map<String, Integer> mCountMap = new HashMap<>();
+
+    @Before("execution(* com..**(..))")
+    public void onActivityMethodBefore2(JoinPoint joinPoint) throws Throwable {
+        String key = joinPoint.getSignature().toString();
+        int count = 0;
+        if (mCountMap.containsKey(key)) {
+            count = mCountMap.get(key);
+        }
+        if (count > 20) {
+            return;
+        }
+
+        Logger.i(key + "\n" + Arrays.toString(joinPoint.getArgs()));
+
+        mCountMap.put(key, count + 1);
+    }
+}
--- app/src/main/java/com/example/librarymanager/BookApplication.kt
+++ app/src/main/java/com/example/librarymanager/BookApplication.kt
@@ -3,12 +3,15 @@ package com.example.librarymanager
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import com.orhanobut.logger.LogLevel
+import com.orhanobut.logger.Logger
 
 class BookApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         context = this
+        Logger.init("RecorderLogger").logLevel(LogLevel.FULL).methodCount(8).hideThreadInfo()
     }
 
     companion object {
